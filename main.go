754c5230b6bcbaf015671082de170a184ec6a133
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-19 22:02:30 +0300

    add connect db sqlite3

--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"log"
 	"math/rand"
+	"os"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+
+func connectDB(dbPath string) (*sql.DB, error) {
+	var db *sql.DB
+	if _, err := os.Stat(dbPath); errors.Is(err, os.ErrNotExist) {
+		db, err = sql.Open("sqlite3", dbPath)
+		if err != nil {
+			return nil, err
+		}
+	
+		sqlStmt := `
+		CREATE TABLE urls (
+			key TEXT NOT NULL UNIQUE,
+			secret_key TEXT NOT NULL UNIQUE,
+			target_url TEXT NOT NULL,
+			is_active BOOLEAN NOT NULL,
+			clicks INTEGER NOT NULL
+		);
+		CREATE UNIQUE INDEX idx_urls_key ON urls (key);
+		CREATE UNIQUE INDEX idx_urls_secret_key ON urls (secret_key);
+		`
+		_, err := db.Exec(sqlStmt)
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlStmt)
+			return nil, err
+		}
+	} else {
+		db, err = sql.Open("sqlite3", dbPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+	return db, nil
+}
 
 func generateURL() string {
 	rand.Seed(time.Now().UnixMicro())
@@ -18,5 +58,8 @@ func generateURL() string {
 }
 
 func main() {
-	print(generateURL())
+	db, err := connectDB("./test.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
