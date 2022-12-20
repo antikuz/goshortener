590089e5a177cc6d7038b371f0866da52db87c63
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-20 20:23:12 +0300

    add ShortenURL model

---
+++ internal/models/models.go
@@ -0,0 +1,9 @@
+package models
+
+type ShortenURL struct {
+	Key        string `json:"key"`
+	Secret_key string `json:"secret_key"`
+	Target_url string `json:"target_url"`
+	Is_active  bool   `json:"is_active"`
+	Clicks     int    `json:"clicks"`
+}
