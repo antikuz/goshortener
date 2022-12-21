881d8fc19c38abe4b954fa5ec5aea4b82e6a7bf3
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-21 19:20:12 +0300

    add db new url

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/antikuz/goshortener/internal/models"
 	_ "github.com/mattn/go-sqlite3"
 	"go.uber.org/zap"
 )
@@ -59,4 +60,34 @@ func connectDB(dbPath string) (*sql.DB, error) {
 
 	}
 	return db, nil
+}
+
+func (s *Storage) NewURL(url models.ShortenURL) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("insert into urls values(?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(url.Key, url.Secret_key, url.Target_url, url.Is_active, url.Clicks)
+	if err != nil {
+		return err
+	}
+	
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Storage) Close() {
+	if err := s.db.Close(); err != nil {
+		s.logger.Sugar().Errorf("Cant close database, due to err: %v", err)
+	}
 }
