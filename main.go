3070bf0480c30858ed2a3c034d971346179ecc5d
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-21 20:21:28 +0300

    add gin router

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/antikuz/goshortener/internal/db"
 	"github.com/antikuz/goshortener/pkg/logging"
+	"github.com/antikuz/goshortener/internal/handlers"
+	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -25,5 +27,16 @@ func generateURL() string {
 func main() {
 	logger := logging.GetLogger()
 	logger.Sugar().Info("Create connection to database")
-	db.NewStorage("./test.sqlite3", logger)
+	database := db.NewStorage("./test.sqlite3", logger)
+	defer database.Close()
+
+	router := gin.Default()
+	handler := handlers.NewHandler(database, logger)
+	
+	router.GET("/:shortUrl", handler.ShortURLRedirect)
+	router.POST("/shorti", handler.ShortURLCreate)
+  
+	router.Run()
 }
+
+
