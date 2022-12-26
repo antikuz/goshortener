27d308ebbdd49444d8e4e34df04fb18ac7ceed2d
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-26 20:41:00 +0300

    add routine for delete expired urls

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/antikuz/goshortener/internal/db"
-	"github.com/antikuz/goshortener/pkg/logging"
 	"github.com/antikuz/goshortener/internal/handlers"
+	"github.com/antikuz/goshortener/pkg/logging"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,6 +15,21 @@ func main() {
 	logger.Sugar().Info("Create connection to database")
 	database := db.NewStorage("./test.sqlite3", logger)
 	defer database.Close()
+
+	go func() {
+		for range time.Tick(time.Hour) {
+			expiredURLs, err := database.GetExpiredURL()
+			if err != nil {
+				logger.Sugar().Errorf("Cannot get expired urls, due to err: %v", err)
+			}
+			for _, url := range expiredURLs {
+				err := database.DeleteURL(url.Key)
+				if err != nil {
+					logger.Sugar().Errorf("Cannot delete expired url with hash %s, due to err: %v", url.Key, err)
+				}
+			}
+		}
+	}()
 
 	router := gin.Default()
 	handler := handlers.NewHandler(database, logger)
