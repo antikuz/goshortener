f699af0f8ff33eed1c15186d8b2a5d2898592832
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-12-26 21:00:23 +0300

    replace gin.run to http.server with context signal

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/antikuz/goshortener/internal/db"
@@ -11,8 +16,16 @@ import (
 )
 
 func main() {
+	ctx, _ := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
 	logger := logging.GetLogger()
-	logger.Sugar().Info("Create connection to database")
+	logger.Info("Create connection to database")
 	database := db.NewStorage("./test.sqlite3", logger)
 	defer database.Close()
 
@@ -35,5 +48,17 @@ func main() {
 	handler := handlers.NewHandler(database, logger)
 	handler.Register(router)
 
-	logger.Sugar().Fatalf("Can't start webserver due to err: %v", router.Run())
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Sugar().Fatalf("Can't start webserver due to err: %v", err)
+		}
+	}()
+
+	<- ctx.Done()
+	logger.Info("Shutting down server...")
 }
